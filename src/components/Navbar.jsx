e553e636a4b057bd670d2395e8824bbbf0f4ce28
Kanna116 <muralikanna77@gmail.com>
2023-11-04 00:04:03 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="nav--icon">
+                    <i className="fa-solid fa-earth-africa"></i>
+                </div>
+                <h1 className="nav--logo">My Travel Journey</h1>
+            </div>
+        </>
+    )
+};
+
+export default Navbar
